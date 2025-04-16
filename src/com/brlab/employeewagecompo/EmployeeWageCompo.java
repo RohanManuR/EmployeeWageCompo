41153bcf10abb73d71cdf4ac33a16d67afdd0a35
rohan <rohanrmanu99@gmail.com>
2025-04-16 21:07:26 +0530

    UC-2 Added Employee wage

--- src/com/brlab/employeewagecompo/EmployeeWageCompo.java
+++ src/com/brlab/employeewagecompo/EmployeeWageCompo.java
@@ -8,25 +8,33 @@ public class EmployeeWageCompo {
         System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
 
         int attendence = 0;
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter the attendence status :\n0 - Absent\n1 - Present");
-        attendence = input.nextInt();
-
+        int dailyWage = 0;
+        int wagePerHour = 20;
+        int fullDayhr = 8;
         /*
-                        OR
+            Scanner input = new Scanner(System.in);
+            System.out.println("Enter the attendence status :\n0 - Absent\n1 - Present");
+            attendence = input.nextInt();
+        */
+
+
+
              Random random = new Random();
              attendence = random.nextInt(2);
-             System.out.println("Random number : "+attendence);
+            // System.out.println("Random number : "+attendence);
 
-                        OR
+        /*              OR
               attendence = (int)(Math.random() * 2);
               System.out.println("Random number : "+attendence);
         */
 
         if(attendence == 0)
             System.out.println("Employee is Absent,.......");
-        else if (attendence == 1)
+        else if (attendence == 1) {
             System.out.println("Employee is Present,......");
+            dailyWage = wagePerHour * fullDayhr;
+            System.out.println("Daily Employee Wage : " + dailyWage);
+        }
         else System.out.println("Sorry invalid number,....");
     }
 }
