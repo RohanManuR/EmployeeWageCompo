d34cef1f4eb94b477081ca3ad6546ffe6346dd8d
rohan <rohanrmanu99@gmail.com>
2025-04-16 16:53:53 +0530

    Added Employee wage computation project

---
+++ src/com/brlab/employeewagecompo/EmployeeWageCompo.java
@@ -0,0 +1,7 @@
+package com.brlab.employeewagecompo;
+
+public class EmployeeWageCompo {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+    }
+}
